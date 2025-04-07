f7efcd5dcff1c3866dea2abb32decc127e2d159c
choice404 <choice404@protonmail.com>
2025-04-06 18:37:03 -0700

    Forgot to remove old addCog function and also forgot to handlethe botbox.conf update

--- cmd/add.go
+++ cmd/add.go
@@ -197,175 +197,24 @@ class %s(commands.Cog):
         return %v
 `, retValue)
 		if err != nil {
+			fmt.Println("Error writing return statement:", err)
 			return
 		}
 	}
 
 	err = file.Sync()
 	if err != nil {
+		fmt.Println("Error syncing file:", err)
 		return
 	}
-}
 
-func oldAddCogs(filename string) error {
-	rootDir, err := FindBotConf()
-	if err != nil {
-		fmt.Println("Error finding bot config:", err)
-		return err
-	}
-	config, err := LoadConfig()
-	Banner()
-
-	if filename == "" {
-		fmt.Println("Eenter the cog name:")
-		fmt.Scanln(&filename)
-	}
-	filePath := filepath.Join(rootDir, "src", "cogs", filename+".py")
-	file, err := os.Create(filePath)
-	if err != nil {
-		fmt.Println(err)
-		return fmt.Errorf("failed to create cog file %s: %w", filename, err)
-	}
-	defer file.Close()
-
-	var commands []CommandInfo
-	var command string
-	var description string
-	var returnType string
-	for {
-		fmt.Println("Enter a command name (or '!!' to finish):")
-		fmt.Scanln(&command)
-		if command == "!!" {
-			break
-		}
-		fmt.Println("Enter a description for the command:")
-		fmt.Scanln(&description)
-
-		var args []ArgInfo
-		var argsInput string
-		var argType string
-		var argDescription string
-		for {
-			fmt.Println("Enter an argument name (or '!!' to finish):")
-			fmt.Scanln(&argsInput)
-			if argsInput == "!!" {
-				break
-			}
-			fmt.Println("Enter the argument type:")
-			fmt.Scanln(&argType)
-			fmt.Println("Enter a description for the argument:")
-			fmt.Scanln(&argDescription)
-			args = append(args, ArgInfo{Name: argsInput, Type: argType, Description: argDescription})
-		}
-
-		fmt.Println("Enter the return type:")
-		fmt.Scanln(&returnType)
-		commands = append(commands, CommandInfo{Name: command, Description: description, Args: args, ReturnType: returnType})
-	}
-
-	className := strings.ToUpper(string(filename[0])) + filename[1:]
-
-	_, err = fmt.Fprintf(file, `"""
-Author %s
-
-%s
-%s
-"""
-
-import discord
-from discord import app_commands
-from discord.ext import commands
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class %s(commands.Cog):
-    def __init__(self, bot) -> None:
-        self.bot = bot
-        print("%s cog loaded")
-`, config.BotInfo.Author, config.BotInfo.Name, config.BotInfo.Description, className, filename)
-	if err != nil {
-		return fmt.Errorf("failed to write initial part of cog file %s: %w", filePath, err)
-	}
-
-	for _, command := range commands {
-		var argBuilder strings.Builder
-		for i, arg := range command.Args {
-			fmt.Fprintf(&argBuilder, "%s: %s", arg.Name, arg.Type)
-			if i < len(command.Args)-1 {
-				argBuilder.WriteString(", ")
-			}
-		}
-		fullArgStr := argBuilder.String()
-
-		_, err = fmt.Fprintf(file, `
-    @app_commands.command(name="%s", description="%s")
-    async def %s(self, interaction: discord.Interaction, %s) -> %s:
-        """
-        %s when the user types "/%s"
-
-            Parameters:
-`, command.Name, command.Description, command.Name, fullArgStr, command.ReturnType, command.Description, command.Name)
-		if err != nil {
-			return fmt.Errorf("writing command signature for %s: %w", command.Name, err)
-		}
-
-		for _, arg := range command.Args {
-			_, err = fmt.Fprintf(file, `                    %s (%s): %s
-`, arg.Name, arg.Type, arg.Description)
-			if err != nil {
-				return fmt.Errorf("writing arg description for %s in %s: %w", arg.Name, command.Name, err)
-			}
-		}
-
-		_, err = fmt.Fprintf(file, `
-            Returns:
-                    %s
-        """
-
-        try:
-            await interaction.response.send_message(f"%s", ephemeral=True)
-        except Exception as e:
-            print(f"Error: {e}")
-            await interaction.response.send_message(f"Error: {e}", ephemeral=True)
-`, command.ReturnType, command.Name)
-		if err != nil {
-			return fmt.Errorf("writing return/try block for %s: %w", command.Name, err)
-		}
-
-		var retValue any
-		switch command.ReturnType {
-		case "str":
-			retValue = `""`
-		case "int":
-			retValue = 0
-		case "float":
-			retValue = 0.0
-		case "bool":
-			retValue = "False"
-		default:
-			retValue = "None"
-		}
-
-		_, err = fmt.Fprintf(file, `
-        return %v
-`, retValue)
-		if err != nil {
-			return fmt.Errorf("writing return statement for %s: %w", command.Name, err)
-		}
-	}
-
-	err = file.Sync()
-	if err != nil {
-		return fmt.Errorf("failed to sync cog file %s: %w", filePath, err)
-	}
 	cog := CogConfig{
 		Name:     strings.ToUpper(string(filename[0])) + filename[1:],
 		File:     strings.ToLower(string(filename[0])) + filename[1:],
 		Commands: []string{},
 	}
 
-	for _, command := range commands {
+	for _, command := range commandList {
 		cog.Commands = append(cog.Commands, command.Name)
 	}
 
@@ -373,15 +222,16 @@ class %s(commands.Cog):
 
 	jsonData, err := json.MarshalIndent(config, "", "    ")
 	if err != nil {
-		return fmt.Errorf("failed to marshal config to JSON: %w", err)
+		fmt.Println("failed to marshal config to JSON: %w", err)
+		return
 	}
 
 	err = os.WriteFile("config.json", jsonData, 0644)
 	if err != nil {
-		return fmt.Errorf("failed to write updated config.json: %w", err)
+		fmt.Println("failed to write updated config.json: %w", err)
+		return
 	}
 
-	return nil
 }
 
 func generateCmdForms() (*huh.Form, *huh.Form) {
